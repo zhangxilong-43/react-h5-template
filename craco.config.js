7095d2638c69c7d314e4501e29e99f8a19c51bc5
zhangxilong-43 <15575055655@163.com>
2024-06-10 17:23:26 +0800

    feat: 微前端适配

--- craco.config.js
+++ craco.config.js
@@ -2,106 +2,107 @@ const path = require('path');
 const { name } = require('./package.json');
 
 const CracoLessPlugin = require('craco-less');
-const postcssPx2Rem = require('postcss-pxtorem')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const postcssPx2Rem = require('postcss-pxtorem');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 const pathResolve = pathUrl => path.join(__dirname, pathUrl);
+const port = 3001;
 
 module.exports = {
-  reactScriptsVersion: 'react-scripts' /* (default value) */,
-  babel: {
+    reactScriptsVersion: 'react-scripts' /* (default value) */,
+    babel: {
+        plugins: [
+            // 生产环境只留console.error、warn，去除console.log
+            [
+                'babel-plugin-transform-remove-console',
+                { exclude: isProd ? ['error', 'warn'] : ['error', 'warn', 'log'] },
+            ],
+        ],
+        presets: [
+            [
+                '@babel/preset-env',
+                {
+                    targets: {
+                        chrome: '49',
+                        ios: '10',
+                    },
+                },
+            ],
+        ],
+    },
+    webpack: {
+        alias: {
+            '@': pathResolve('src'),
+            '@assets': pathResolve('src/assets'),
+            '@components': pathResolve('src/components'),
+            '@constants': pathResolve('src/constants'),
+            '@containers': pathResolve('src/containers'),
+            '@hooks': pathResolve('src/hooks'),
+            '@mocks': pathResolve('src/mocks'),
+            '@routes': pathResolve('src/routes'),
+            '@services': pathResolve('src/services'),
+            '@styles': pathResolve('src/styles'),
+            '@types': pathResolve('src/types'),
+            '@utils': pathResolve('src/utils'),
+            '@contexts': pathResolve('src/contexts'),
+            '@static': pathResolve('src/static'),
+            '@pages': pathResolve('src/pages'),
+        },
+        configure(webpackConfig) {
+            // 配置扩展扩展名
+            webpackConfig.resolve.extensions = [...webpackConfig.resolve.extensions, ...['.scss', '.css']];
+            webpackConfig.output.publicPath = `http://localhost:${port}/`; // 作为 micro-app 使用时需要添加该字段
+            webpackConfig.output.library = `${name}-[name]`;
+            webpackConfig.output.libraryTarget = 'umd';
+            webpackConfig.output.globalObject = 'window';
+            return webpackConfig;
+        },
+    },
     plugins: [
-      // 生产环境只留console.error、warn，去除console.log
-      [
-        'babel-plugin-transform-remove-console',
-        { exclude: isProd ? ['error', 'warn'] : ['error', 'warn', 'log'] }
-      ]
-    ],
-    presets: [
-      [
-        "@babel/preset-env",
         {
-          "targets": {
-            "chrome": "49",
-            "ios": "10"
-          }
-        }
-      ]
-    ]
-  },
-  webpack: {
-    alias: {
-      '@': pathResolve('src'),
-      '@assets': pathResolve('src/assets'),
-      '@components': pathResolve('src/components'),
-      '@constants': pathResolve('src/constants'),
-      '@containers': pathResolve('src/containers'),
-      '@hooks': pathResolve('src/hooks'),
-      '@mocks': pathResolve('src/mocks'),
-      '@routes': pathResolve('src/routes'),
-      '@services': pathResolve('src/services'),
-      '@styles': pathResolve('src/styles'),
-      '@types': pathResolve('src/types'),
-      '@utils': pathResolve('src/utils'),
-      '@contexts': pathResolve('src/contexts'),
-      '@static': pathResolve('src/static'),
-      '@pages': pathResolve('src/pages'),
+            plugin: CracoLessPlugin,
+            options: {
+                lessLoaderOptions: {
+                    // 配置可以参照webpack的less-loader具体配置
+                    lessOptions: {
+                        javascriptEnabled: true, // 允许less文件中使用js表达式
+                    },
+                },
+            },
+        },
+        {
+            plugin: BundleAnalyzerPlugin,
+        },
+    ],
+    style: {
+        postcss: {
+            mode: 'extends',
+            loaderOptions: () => {
+                return {
+                    postcssOptions: {
+                        ident: 'postcss',
+                        config: false,
+                        plugins: [
+                            postcssPx2Rem({
+                                rootValue: 37.5, // 设计稿尺寸/10
+                                propList: ['*'], // 需要转换的样式属性，默认为 ['*']，即匹配所有属性
+                                exclude: /node_modules/i, // 排除掉node_modules中转换
+                            }),
+                        ],
+                    },
+                    sourceMap: false,
+                };
+            },
+        },
     },
-    configure(webpackConfig) {
-      // 配置扩展扩展名
-      webpackConfig.resolve.extensions = [...webpackConfig.resolve.extensions, ...['.scss', '.css']];
-      // 接入微前端框架qiankun的配置,不接入微前端可以不需要
-      webpackConfig.output.library = `${name}-[name]`;
-      webpackConfig.output.libraryTarget = 'umd';
-      webpackConfig.output.globalObject = 'window';
-      return webpackConfig;
+    devServer: {
+        // 本地服务的端口号
+        port,
+        // 本地服务的响应头设置
+        headers: {
+            // 允许跨域
+            'Access-Control-Allow-Origin': '*',
+        },
     },
-  },
-  plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: { // 配置可以参照webpack的less-loader具体配置
-          lessOptions: {
-            javascriptEnabled: true // 允许less文件中使用js表达式
-          }
-        }
-      }
-    },
-    {
-      plugin: BundleAnalyzerPlugin
-    }
-  ],
-  style: {
-    postcss: {
-      mode: 'extends',
-      loaderOptions: () => {
-        return {
-          postcssOptions: {
-            ident: 'postcss',
-            config: false,
-            plugins: [
-              postcssPx2Rem({
-                rootValue: 37.5, // 设计稿尺寸/10
-                propList: ['*'], // 需要转换的样式属性，默认为 ['*']，即匹配所有属性
-                exclude: /node_modules/i // 排除掉node_modules中转换
-              })
-            ]
-          },
-          sourceMap: false
-        }
-      }
-    }
-  },
-  devServer: {
-    // 本地服务的端口号
-    port: 3001,
-    // 本地服务的响应头设置
-    headers: {
-      // 允许跨域
-      'Access-Control-Allow-Origin': '*',
-    },
-  },
 };
-
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import '@styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './public-path';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
